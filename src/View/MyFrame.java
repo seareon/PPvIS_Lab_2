40425b04cf3be32fe24abbcebbe1dcc4b285ac4f
Mike Ro <seareon1587.mr@gmail.com>
2016-03-26 13:49:41 +0300

    v 1.0

--- src/View/MyFrame.java
+++ src/View/MyFrame.java
@@ -1,6 +1,8 @@
 package View;
 
-public class MyFrame {
+import javax.swing.JFrame;
+
+public class MyFrame extends JFrame {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
